84680aebb08a8fd96606891569a07198de69d9a1
katungi <dankatdennis@gmail.com>
2020-04-12 11:23:56 +0300

    added output scope

--- src/estimator.js
+++ src/estimator.js
@@ -1,27 +1,46 @@
 
 const covid19ImpactEstimator = (data) => {
-  const { reportedCases, timePassed, period } = data;
+  const {
+    reportedCases,
+    timeToElapse,
+    periodType
+  } = data;
+
+  const impact = {};
+  const severeImpact = {};
+  // challenge 1
 
   const currentlyInfected = reportedCases * 10;
   const currentInfections = reportedCases * 50;
 
-  // check if the timePassed in in days weeks or months
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentInfections = reportedCases * 50;
+
+
+  // check if the timeToElapse in in days weeks or months
   let timeFactor;
   // eslint-disable-next-line no-empty
-  switch (period) {
+  switch (periodType) {
     case 'month':
-      timeFactor = Math.trunc(timePassed / 3) * 30;
+      timeFactor = Math.trunc(timeToElapse / 3) * 30;
       break;
     case 'week':
-      timeFactor = Math.trunc(timePassed / 3) * 7;
+      timeFactor = Math.trunc(timeToElapse / 3) * 7;
       break;
     default:
-      timeFactor = Math.trunc(timePassed / 3);
+      timeFactor = Math.trunc(timeToElapse / 3);
       break;
   }
   // time passed as infection rates grow
 
   const infectedByRequestedTime = timeFactor ** 2;
+
+  impact.infectedByRequestedTime = timeFactor ** 2;
+  severeImpact.infectedByRequestedTime = timeFactor ** 2;
+
+  // challenge 2
+
+  impact.severeCasesByRequestedTime = infectedByRequestedTime * 0.15;
 
 
   return {
