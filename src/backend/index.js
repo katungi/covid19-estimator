bc523b6d159f86075700e0f6996069f92e398aff
katungi <dankatdennis@gmail.com>
2020-04-14 19:02:42 +0300

    added js2xmlparser

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
-const estimator = require('../estimator');
+const js2xmlparser = require('js2xmlparser');
+
+const covid19ImpactEstimator = require('../estimator');
 
 const app = express();
 
 app.post('/api/v1/on-covid-19/json', (req, res) => {
-  const output = estimator(req.body);
+  const output = covid19ImpactEstimator(req.body);
   res.type('application/json');
   res.send(output);
+});
+
+app.post('/api/v1/on-covid-19/xml', (req, res) => {
+  const output = covid19ImpactEstimator(req.body);
+  res.type('application/xml');
+
 });
 
 
