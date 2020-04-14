c83b491fc70cc0b694100526c7d0924f38f26082
katungi <dankatdennis@gmail.com>
2020-04-14 21:01:30 +0300

    added response time

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const js2xmlparser = require('js2xmlparser');
 
+const responseTime = require('response-time');
+
 const covid19ImpactEstimator = require('../estimator');
 
 const app = express();
+
+app.use(express.json());
+app.use(responseTime());
 
 app.post('/api/v1/on-covid-19/json', (req, res, next) => {
   const output = covid19ImpactEstimator(req.body);
@@ -14,9 +19,17 @@ app.post('/api/v1/on-covid-19/json', (req, res, next) => {
 });
 
 app.post('/api/v1/on-covid-19/xml', (req, res, next) => {
-  const output = covid19ImpactEstimator(req.body);
+  res.header('Content-Type', 'application/xml');
   res.type('application/xml');
-  res.send(js2xmlparser.parse(output));
+  const output = covid19ImpactEstimator(req.body);
+  const estimate = js2xmlparser.parse('estimate', output);
+  res.send(estimate);
+  next();
+});
+
+app.get('/api/v1/on-covid-19/logs', (req, res, next) => {
+  res.header('Content-Type', 'text/plain; charset=utf-8');
+  res.send('Hello world');
   next();
 });
 
